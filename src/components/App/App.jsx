fe6eb2a9733abc4910b23ee92b496cd3aca210ea
Grave <kyuvcpavm@mail.ru>
2022-05-06 21:24:30 +0300

    Созданы первоначальные компоненты

---
+++ src/components/App/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import React from "react";
+import Main from '../Main/Main';
+import Header from '../Header/Header';
+
+
+function App() {
+  return (
+    <div className="page">
+      <Header/>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
